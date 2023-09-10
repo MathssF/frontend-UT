3e78bc8e6d624c15975ab4a049caf80cbaa89c0a
Matheus Flores <Maths.Aki@gmail.com>
2023-09-09 21:49:38 -0300

    fetch no context

--- contexts/genresContext.js
+++ contexts/genresContext.js
@@ -6,6 +6,19 @@ const GenresProvider = ({ children }) => {
   const [genres, setGenres] = useState([]);
 
   useEffect(() => {
+    const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjcxZWQxMzcwZmJkODliMWYwZTdlZjY5N2FkYjk4ZSIsInN1YiI6IjY0ZDgzNjQ2MDAxYmJkMDBjNmM3M2NjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0GsCxynMfYUeSw03wyxd1BpvE5T6IdFKuQmYuG-Ap-0'
+      }
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(data => setGenres(data.genres))
+      .catch(err => console.error('error:' + err));
   }, []);
 
   return (
