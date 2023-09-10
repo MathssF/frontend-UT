9e697d6c3df5fbd29307ab3d046dc2b2c1114d07
Matheus Flores <Maths.Aki@gmail.com>
2023-09-10 02:00:00 -0300

    save and back

--- utils/genres.js
+++ utils/genres.js
@@ -1,15 +1,18 @@
-// const fetch = require('node-fetch');
+import React, { useContext } from 'react';
+import { GenresContext } from '../contexts/genresContext';
 
-//   const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       accept: 'application/json',
-//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjcxZWQxMzcwZmJkODliMWYwZTdlZjY5N2FkYjk4ZSIsInN1YiI6IjY0ZDgzNjQ2MDAxYmJkMDBjNmM3M2NjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0GsCxynMfYUeSw03wyxd1BpvE5T6IdFKuQmYuG-Ap-0'
-//     }
-//   };
+export default function GenresList() {
+  const { genres } = useContext(GenresContext);
 
-//   const genres = fetch(url, options)
-//     .then(res => res.json())
-//     .then(json => console.log(json))
-//     .catch(err => console.error('error:' + err));
+  const ListGen = { genres.map((index) => (
+    <button
+      key={index.id}
+    >
+      { index.name }
+    </button>
+  )) }
+
+  return (
+    { ListGen }
+  )
+}
