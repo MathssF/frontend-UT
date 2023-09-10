5408078a3228d6e6cde7284df2e30abebeab2567
Matheus Flores <Maths.Aki@gmail.com>
2023-09-10 01:50:28 -0300

    criando o botão

--- pages/desktop.js
+++ pages/desktop.js
@@ -2,22 +2,6 @@ import React, { useContext } from 'react';
 import { GenresContext } from '../contexts/genresContext';
 
 export default function Desktop() {
-  // const fetch = require('node-fetch');
-
-  // const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjcxZWQxMzcwZmJkODliMWYwZTdlZjY5N2FkYjk4ZSIsInN1YiI6IjY0ZDgzNjQ2MDAxYmJkMDBjNmM3M2NjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0GsCxynMfYUeSw03wyxd1BpvE5T6IdFKuQmYuG-Ap-0'
-  //   }
-  // };
-
-  // const genres = fetch(url, options)
-  //   .then(res => res.json())
-  //   .then(json => console.log(json))
-  //   .catch(err => console.error('error:' + err));
-
   const { genres } = useContext(GenresContext);
   
   return (
@@ -30,7 +14,13 @@ export default function Desktop() {
 
       <main>
         <p>Milhões de filmes, séries e pessoas para descobrir. Explore já.</p>
-
+        { genres.map((index) => (
+          <button
+            key={index.id}
+          >
+            { index.name }
+          </button>
+        )) }
       </main>
 
       <footer>
