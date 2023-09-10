7c250cde77bbd7c22b0616ef904bbe05e988e9c0
Matheus Flores <Maths.Aki@gmail.com>
2023-09-10 01:38:52 -0300

    progredindo

--- utils/genres.js
+++ utils/genres.js
@@ -1,15 +1,15 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-  const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjcxZWQxMzcwZmJkODliMWYwZTdlZjY5N2FkYjk4ZSIsInN1YiI6IjY0ZDgzNjQ2MDAxYmJkMDBjNmM3M2NjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0GsCxynMfYUeSw03wyxd1BpvE5T6IdFKuQmYuG-Ap-0'
-    }
-  };
+//   const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+//   const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjcxZWQxMzcwZmJkODliMWYwZTdlZjY5N2FkYjk4ZSIsInN1YiI6IjY0ZDgzNjQ2MDAxYmJkMDBjNmM3M2NjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0GsCxynMfYUeSw03wyxd1BpvE5T6IdFKuQmYuG-Ap-0'
+//     }
+//   };
 
-  const genres = fetch(url, options)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error('error:' + err));
+//   const genres = fetch(url, options)
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+//     .catch(err => console.error('error:' + err));
