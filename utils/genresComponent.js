4a90f8b5c2561bf920c85545ebb1b88841650917
Matheus Flores <Maths.Aki@gmail.com>
2023-09-14 01:44:40 -0300

    fix comments

--- utils/genresComponent.js
+++ utils/genresComponent.js
@@ -1,9 +1,6 @@
 import React from 'react';
-// import React, { useContext } from 'react';
-// import { GenresContext } from '../contexts/genresContext';
 
 export default function GenresComponent({ genres, onGenreSelect }) {
-  // const { genres } = useContext(GenresContext);
 
   return (
     <div>
