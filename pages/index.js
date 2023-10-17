d7c54f643aabe8ca516cbeab29bd4fca90b15310
Matheus Flores <Maths.Aki@gmail.com>
2023-10-17 00:01:47 -0300

    continua

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
-// import { GenresContext, GenresProvider } from '../contexts/genresContext';
+import { GenresContext, GenresProvider } from '../contexts/genresContext';
 // import { GenresComponent } from '../utils/genresComponent.js';
-import { GenresProvider } from '../contexts/genresContext';
+// import { GenresProvider } from '../contexts/genresContext';
 import Desktop from '../utils/Desktop';
 
 export default function IndexMenu() {
-  const { genres, setGenres } = useContext(GenresContext);
+  // const { genres, setGenres } = useContext(GenresContext);
 
-  const handleGenreSelect = (id) => {
-    setGenres(prevGenres => prevGenres.map(genre =>
-        genre.id === id? { ...genre, select: !genre.select} : genre
-      ))
-  }
+  // const handleGenreSelect = (id) => {
+  //   setGenres(prevGenres => prevGenres.map(genre =>
+  //       genre.id === id? { ...genre, select: !genre.select} : genre
+  //     ))
+  // }
   
   return (
     <GenresProvider>
