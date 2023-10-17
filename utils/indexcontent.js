2219c37ba6bbe2c2ae9782b05d3a7c24758cf43f
Matheus Flores <Maths.Aki@gmail.com>
2023-10-16 23:18:11 -0300

    indexcontent

---
+++ utils/indexcontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Desktop() {
+  // Começando esta parte aqui
+  return (
+    <div>
+      <p>
+        Milhões de filmes, séries e pessoas para descobrir. Explore já.
+      </p>
+    </div>
+  );
+}
