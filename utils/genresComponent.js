01a45f8ef7fb50a61817089e1dfc7cc8d18397ed
Matheus Flores <Maths.Aki@gmail.com>
2023-11-08 11:58:55 -0300

    prosseguindo

--- utils/genresComponent.js
+++ utils/genresComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 export default function GenresComponent({ genres, onGenreSelect }) {
 
-  // console.log(genres);
+  console.log(genres);
   return (
     <div>
       { genres.map((genre) => (
         <div
           key={genre.id}
+          style={genre.select ? "background-color: blue" : "background-color: white"}
         >
           <button
             onClick={() => onGenreSelect(genre.id)}
