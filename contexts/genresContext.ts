4c2154dc5343d09ad29ca45109c91df460875c9b
Matheus Flores <Maths.Aki@gmail.com>
2023-11-09 16:10:53 -0300

    ponto

--- contexts/genresContext.ts
+++ contexts/genresContext.ts
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState, ReactNode } from 'react';
 interface Genre {
   id: number;
   name: string;
-  select: boolean;
+  select?: boolean;
 }
 
 interface GenresContextProps {
