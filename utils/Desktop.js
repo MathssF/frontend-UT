2aecd5603ecad6eb1e243732f67aad92eab9c0d9
Matheus Flores <Maths.Aki@gmail.com>
2023-11-22 11:23:32 -0300

    criando o contexto dos titulos

--- utils/Desktop.js
+++ utils/Desktop.js
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react';
 import { GenresContext, GenresProvider } from '../contexts/genresContext';
 import GenresComponent from './genresComponent';
-// import { GenresContext } from '../contexts/genresContext';
-// import { GenresComponent } from '../utils/genresComponent.js';
 
 export default function Desktop() {
   const { genres, setGenres } = useContext(GenresContext);
