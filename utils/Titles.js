3332405255184c8a247c636fb555ed3191d887a1
Matheus Flores <Maths.Aki@gmail.com>
2024-01-03 15:38:33 -0300

    linhas

--- utils/Titles.js
+++ utils/Titles.js
@@ -14,27 +14,42 @@ export default function Titles({ genres }) {
   if (!Array.isArray(titles) || titles.length === 0) {
     return <div><h1>Nenhum título disponível</h1></div>;
   }
+
   const filteredTitles = genres.length > 0
     ? titles.filter(title => title.genre_ids.some(genreId => genres.includes(genreId)))
     : titles;
 
-    return (
-      <div>
-        {filteredTitles.map(title => (
-          <div key={title.id} style={{ maxWidth: '176px' }}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${title.poster_path}`}
-              alt={title.title}
-              style={{ maxWidth: '100%', height: 'auto' }}
-            />
-            {title.title}
-            {new Date(title.release_date).toLocaleDateString('pt-BR', {
-              day: 'numeric',
-              month: 'short',
-              year: 'numeric',
-            })}
-          </div>
-        ))}
-      </div>
-    );
+  const containerStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+  };
+
+  const titleStyle = {
+    boxSizing: 'border-box',
+    maxWidth: '176px',
+    marginRight: '32px',
+    marginBottom: '48px',
+  };
+
+  return (
+    <div style={containerStyle}>
+      {filteredTitles.map(title => (
+        <div key={title.id} style={titleStyle}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${title.poster_path}`}
+            alt={title.title}
+            style={{ maxWidth: '100%', height: 'auto' }}
+          />
+          <br />
+          {title.title}
+          <br />
+          {new Date(title.release_date).toLocaleDateString('pt-BR', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+          })}
+        </div>
+      ))}
+    </div>
+  );
 }
