0e3fb77e0a7e76c27c18e6af143299ee59e6944a
Matheus Flores <Maths.Aki@gmail.com>
2024-01-11 15:23:07 -0300

    if

--- pages/[movie_id].js
+++ pages/[movie_id].js
@@ -1,27 +1,10 @@
-// import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function idFilm() {
   const router = useRouter();
   const { movie_id } = router.query;
   const [filmContent, setFilmContent] = useState(null);
-
-  // const fetch = require('node-fetch');
-
-  // const url = `https://api.themoviedb.org/3/movie/${movie_id}?language=en-US`;
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjcxZWQxMzcwZmJkODliMWYwZTdlZjY5N2FkYjk4ZSIsInN1YiI6IjY0ZDgzNjQ2MDAxYmJkMDBjNmM3M2NjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0GsCxynMfYUeSw03wyxd1BpvE5T6IdFKuQmYuG-Ap-0'
-  //   }
-  // };
-
-  // const film_content = fetch(url, options)
-  //   .then(res => res.json())
-  //   .then(json => console.log(json))
-  //   .catch(err => console.error('error:' + err));
 
   useEffect(() => {
     const fetchDataFilm = async () => {
@@ -37,9 +20,9 @@ export default function idFilm() {
       const response = await fetch(url, options);
       const data = await response.json();
       setFilmContent(data);
-      if (movie_id) {
-        fetchDataFilm();
-      }
+      // if (movie_id) {
+      //   fetchDataFilm();
+      // }
     }
   }, [movie_id]);
 
