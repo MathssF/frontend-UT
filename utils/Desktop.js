c2b56878ecf93e01796f7d2d22ea9db7a5cc9d82
Matheus Flores <Maths.Aki@gmail.com>
2024-01-16 06:34:45 -0300

    import

--- utils/Desktop.js
+++ utils/Desktop.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { GenresContext } from '../contexts/genresContext';
 import GenresComponent from './genresComponent';
 import Titles from './Titles'
+// import './images/tmdbLogo.svg';
 
 export default function Desktop() {
   const { genres, setGenres } = useContext(GenresContext);
