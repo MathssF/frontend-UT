9f887ba50a1c267eecd348fb11d142c45cc24ed4
Matheus Flores <Maths.Aki@gmail.com>
2024-01-23 05:58:35 -0300

    styleMovieId

--- pages/[movie_id].js
+++ pages/[movie_id].js
@@ -73,20 +73,48 @@ export default function idFilm() {
         }}
         >
           <div
-            style={{
-              margin: '70px 112px 0 0',
-            }}
-          >
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${filmContent.poster_path}`}
-              alt={filmContent.title}
+          style={{
+            backgroundColor: '#861040',
+            width: '100%',
+          }}>
+            <div
               style={{
-                maxWidth: '186px',
-                height: 'auto',
+                padding: '70px 0 0 112px',
+                display: 'flex',
+                // justifyContent: 'space-between',
+                // gap: '33px',
               }}
-            />
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${filmContent.poster_path}`}
+                alt={filmContent.title}
+                style={{
+                  maxWidth: '383px',
+                  height: 'auto',
+                  marginRight: '33px',
+                  marginBottom: '-46px'
+                }}
+              />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+              <h1  style={{
+                fontWeight: 'bold',
+                fontSize: '32px',
+                color: '#ffffff',
+                fontFamily: 'Roboto, sans-serif',
+              }}>
+                {filmContent.title}
+               </h1>
+               <p></p>
+              </div>
+            </div>
           </div>
-          <h1 style={{ textAlign: 'center' }}>{filmContent.title}</h1>
+          
+
         </div>
       ) : <div style={{ textAlign: 'center' }}>Erro de API</div>}
     </div>
